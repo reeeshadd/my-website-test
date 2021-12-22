b44f7e06e446bd0b76a11624ac4af00cf9455f57
Reshad Hosen <59388486+reeeshadd@users.noreply.github.com>
2021-12-23 00:39:59 +0600

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ let count = 0
 
 function increment() {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
 }
 
 function save() {
     let entry = count + " - "
     saveEl.textContent += entry
+    countEl.textContent = 0
+    count = 0
 }
 
