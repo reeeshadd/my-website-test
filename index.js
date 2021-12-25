c0c9f3fbf6f68c851a33ce590ba951aea04e7e3e
Reshad Hosen <59388486+reeeshadd@users.noreply.github.com>
2021-12-25 19:47:35 +0600

    added total sum feature

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
+let sumEl = document.getElementById("sum-el")
 
 let count = 0
+let sum = 0
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+
 function save() {
     let entry = count + " - "
     saveEl.textContent += entry
+    sum += count
+    sumEl.textContent = "Sum: " + sum
     countEl.textContent = 0
     count = 0
 }
